3d6a6248f97c516dd9161db4f5ede8ce62f4e4aa
songgx <guanxin@7net.cc>
2018-11-28 14:51:55 +0800

    提交练习

--- app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice08XfermodeView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice08XfermodeView.java
@@ -1,20 +1,28 @@
 package com.hencoder.hencoderpracticedraw2.practice;
 
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Xfermode;
+import android.os.Build;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
-import com.hencoder.hencoderpracticedraw2.R;
+import com.example.sgx.customviewsummary.R;
 
 public class Practice08XfermodeView extends View {
     Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
     Bitmap bitmap1;
     Bitmap bitmap2;
+    Xfermode xfermode1 = new PorterDuffXfermode(PorterDuff.Mode.SRC);
+    Xfermode xfermode2 = new PorterDuffXfermode(PorterDuff.Mode.DST_IN);
+    Xfermode xfermode3 = new PorterDuffXfermode(PorterDuff.Mode.DST_OUT);
 
     public Practice08XfermodeView(Context context) {
         super(context);
@@ -31,8 +39,10 @@ public class Practice08XfermodeView extends View {
     {
         bitmap1 = BitmapFactory.decodeResource(getResources(), R.drawable.batman);
         bitmap2 = BitmapFactory.decodeResource(getResources(), R.drawable.batman_logo);
+
     }
 
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
@@ -41,18 +51,28 @@ public class Practice08XfermodeView extends View {
 
         // 别忘了用 canvas.saveLayer() 开启 off-screen buffer
 
+        int saveLayer = canvas.saveLayer(null, paint);
+
         canvas.drawBitmap(bitmap1, 0, 0, paint);
         // 第一个：PorterDuff.Mode.SRC
+        paint.setXfermode(xfermode1);
         canvas.drawBitmap(bitmap2, 0, 0, paint);
+        paint.setXfermode(null);
 
         canvas.drawBitmap(bitmap1, bitmap1.getWidth() + 100, 0, paint);
         // 第二个：PorterDuff.Mode.DST_IN
+        paint.setXfermode(xfermode2);
         canvas.drawBitmap(bitmap2, bitmap1.getWidth() + 100, 0, paint);
+        paint.setXfermode(null);
+
 
         canvas.drawBitmap(bitmap1, 0, bitmap1.getHeight() + 20, paint);
         // 第三个：PorterDuff.Mode.DST_OUT
+        paint.setXfermode(xfermode3);
         canvas.drawBitmap(bitmap2, 0, bitmap1.getHeight() + 20, paint);
-
+        paint.setXfermode(null);
         // 用完之后使用 canvas.restore() 恢复 off-screen buffer
+
+        canvas.restoreToCount(saveLayer);
     }
 }
